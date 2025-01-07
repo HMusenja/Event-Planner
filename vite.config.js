2a3fb4e2221b39d217fe877fa675f05d90321560
HMusenja <musenjahumphrey@yahoo.com>
2025-01-07 10:05:29 +0100

    updated viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { ghPages } from 'vite-plugin-gh-pages';
-import { staticPages } from 'vite-plugin-static-pages';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages(),staticPages()],
+  plugins: [react(), ghPages()],
   base: '/Event-Search-Planner/',
   css: {
     postcss: './postcss.config.js', // Ensures PostCSS config is loaded
