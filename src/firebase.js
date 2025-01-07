359c9226a8f8f6dcb2121ade4f5fb3816e9e336f
HMusenja <musenjahumphrey@yahoo.com>
2025-01-07 08:39:36 +0100

    updated env

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCAI2nZzcRP09Vmugb61ztxKZG1FC9lpYg",
-  authDomain: "event-planner-f074c.firebaseapp.com",
-  projectId: "event-planner-f074c",
-  storageBucket: "event-planner-f074c.appspot.com",
-  messagingSenderId: "228038484139",
-  appId: "1:228038484139:web:55676fda81a746d38757ee",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
